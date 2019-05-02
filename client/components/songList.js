537f499e5d6524491c156976df207602572fc483
Fernando Brandao <fwbrandao@gmail.com>
2019-05-02 08:36:51 +0100

    renderSongs()

--- client/components/songList.js
+++ client/components/songList.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+
+    renderSongs() {
+        return this.props.data.songs.map(song => {
+            return (
+            <li key={song.id}>
+                {song.title}
+            </li>
+            );
+        });
+    }
+
     render() {
+        if (this.props.data.loading) {
+            return  <div>
+                        Loading...
+                    </div>;
+        }
+
         return (
             <div>
-                SongList
+                {this.renderSongs()}
             </div>
         );
     }
@@ -14,9 +32,10 @@ class SongList extends Component {
 const query = gql`
     {
         songs {
+            id
             title
         }
     }
 `;
 
-export default SongList;
+export default graphql(query)(SongList);
