7084c9a2484c53302a332a6745595fb5cadf7d44
Fernando Brandao <fwbrandao@gmail.com>
2019-05-02 07:49:19 +0100

    added Apollo

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://lyricaluser:01052019Change@ds149676.mlab.com:49676/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
